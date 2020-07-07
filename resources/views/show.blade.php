744cacc17de0fdc1369a5e06c5835dcf3b4e8c58
BATCHI <claude.batchi@epitech.eu>
2020-07-07 17:10:14 +0200

    Add modal play trailer

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -43,34 +43,45 @@
           </div>
         </div>
 
-        @if(count($movie['videos']['results']) > 0)
-          <div class="mt-12">
-            <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}" target="_blank"
-             class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-4 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-              <i class="fa fa-play-circle w-6 text-2xl" aria-hidden="true"></i>
-              <span class="ml-2">Play Trailer</span>
-            </a>
-          </div><!-- Fin de info -->
-        @endif
+        <div x-data="{ isOpen: false }">
+          @if(count($movie['videos']['results']) > 0)
+            <div class="mt-12">
 
-        <div
-          style="background-color: rgba(0, 0, 0, .5)"
-          class="btop-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
-        >
-          <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-            <div class="bg-gray-900 rounded">
-              <div class="flex justify-end pr-4 pt-2">
-                <button class="text-3xl leading-none hover:text-gray-300">&times;</button>
-                <div class="modal-body px-8 py-8">
-                  <div style="padding-top: 56.25%" class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+              <button href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}"
+                @click="isOpen = true"
+                target="_blank"
+                class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
+              >
+                <i class="fa fa-play-circle w-6 text-2xl" aria-hidden="true"></i>
+                <span class="ml-2">Play Trailer</span>
+              </button>
+            </div><!-- Fin de info -->
+          @endif
 
+          <template x-if="isOpen">
+            <div
+              style="background-color: rgba(0, 0, 0, .5);"
+              class="absolute top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+            >
+              <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                <div class="bg-gray-900 rounded">
+                  <div class="flex justify-end pr-4 pt-2">
+                    <button
+                      @click="isOpen = false"
+                      @keydown.escape.window="isOpen = false"
+                      class="text-3xl leading-none hover:text-gray-300">&times;
+                    </button>
+                  </div>
+                  <div class="modal-body px-8 py-8">
+                    <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                      <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </template>
         </div>
-
       </div>
     </div>
 
